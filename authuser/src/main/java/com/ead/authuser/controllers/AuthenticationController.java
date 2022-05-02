ce06695e167bd33a0454f32976d7928c97a34411
francitonioferreiraalves <francitonio@gmail.com>
2022-05-02 14:07:43 -0300

    implementando autorização e autenticação

--- authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -1,21 +1,32 @@
 package com.ead.authuser.controllers;
 
-import com.ead.authuser.dtos.UserDto;
-import com.ead.authuser.enums.UserStatus;
-import com.ead.authuser.enums.UserType;
-import com.ead.authuser.models.UserModel;
-import com.ead.authuser.services.UserService;
-import com.fasterxml.jackson.annotation.JsonView;
-import lombok.extern.log4j.Log4j2;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import com.ead.authuser.dtos.UserDto;
+import com.ead.authuser.enums.RoleType;
+import com.ead.authuser.enums.UserStatus;
+import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.RoleModel;
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.RoleService;
+import com.ead.authuser.services.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
+
+import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @RestController
@@ -23,40 +34,50 @@ import java.time.ZoneId;
 @RequestMapping("/auth")
 public class AuthenticationController {
 
-    @Autowired
-    UserService userService;
+	@Autowired
+	UserService userService;
 
-    @PostMapping("/signup")
-    public ResponseEntity<Object> registerUser(@RequestBody @Validated(UserDto.UserView.RegistrationPost.class)
-                                                   @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto){
-        log.debug("POST registerUser userDto received {} ", userDto.toString());
-        if(userService.existsByUsername(userDto.getUsername())){
-            log.warn("Username {} is Already Taken ", userDto.getUsername());
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is Already Taken!");
-        }
-        if(userService.existsByEmail(userDto.getEmail())){
-            log.warn("Email {} is Already Taken ", userDto.getEmail());
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is Already Taken!");
-        }
-        var userModel = new UserModel();
-        BeanUtils.copyProperties(userDto, userModel);
-        userModel.setUserStatus(UserStatus.ACTIVE);
-        userModel.setUserType(UserType.STUDENT);
-        userModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
-        userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
-        userService.saveUser(userModel);
-        log.debug("POST registerUser userId saved {} ", userModel.getUserId());
-        log.info("User saved successfully userId {} ", userModel.getUserId());
-        return  ResponseEntity.status(HttpStatus.CREATED).body(userModel);
-    }
+	@Autowired
+	RoleService roleService;
 
-    @GetMapping("/")
-    public String index(){
-        log.trace("TRACE");
-        log.debug("DEBUG");
-        log.info("INFO");
-        log.warn("WARN");
-        log.error("ERROR");
-        return "Logging Spring Boot...";
-    }
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	@PostMapping("/signup")
+	public ResponseEntity<Object> registerUser(
+			@RequestBody @Validated(UserDto.UserView.RegistrationPost.class) @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto) {
+		log.debug("POST registerUser userDto received {} ", userDto.toString());
+		if (userService.existsByUsername(userDto.getUsername())) {
+			log.warn("Username {} is Already Taken ", userDto.getUsername());
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is Already Taken!");
+		}
+		if (userService.existsByEmail(userDto.getEmail())) {
+			log.warn("Email {} is Already Taken ", userDto.getEmail());
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is Already Taken!");
+		}
+		RoleModel roleModel = roleService.findByRoleName(RoleType.ROLE_STUDENT)
+				.orElseThrow(() -> new RuntimeException("Error: Role is Not Found."));
+		userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
+		var userModel = new UserModel();
+		BeanUtils.copyProperties(userDto, userModel);
+		userModel.setUserStatus(UserStatus.ACTIVE);
+		userModel.setUserType(UserType.STUDENT);
+		userModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+		userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+		userModel.getRoles().add(roleModel);
+		userService.saveUser(userModel);
+		log.debug("POST registerUser userId saved {} ", userModel.getUserId());
+		log.info("User saved successfully userId {} ", userModel.getUserId());
+		return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		log.trace("TRACE");
+		log.debug("DEBUG");
+		log.info("INFO");
+		log.warn("WARN");
+		log.error("ERROR");
+		return "Logging Spring Boot...";
+	}
 }
